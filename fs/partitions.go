d371501771a4f1d8e0075529c9b83f7eec5f5cff
Tom <twitchyliquid64@ciphersink.net>
2019-07-10 13:21:24 -0700

    Start implementing fs package

---
+++ fs/partitions.go
@@ -0,0 +1,43 @@
+// Package fs exposes access to filesystems.
+package fs
+
+import (
+	"fmt"
+
+	"github.com/rekby/mbr"
+)
+
+// Partition types
+const (
+	PartitionTypeFAT32LBA             = 0xc
+	PartitionTypeLinuxNativePartition = 0x83
+)
+
+// CheckPiPartitionTable returns an error if the pi image has a bad partition table.
+func CheckPiPartitionTable(t *mbr.MBR) error {
+	if err := t.Check(); err != nil {
+		return err
+	}
+	partitions := t.GetAllPartitions()
+
+	partitionSpec := []struct {
+		Type mbr.PartitionType
+	}{
+		{Type: PartitionTypeFAT32LBA},
+		{Type: PartitionTypeLinuxNativePartition},
+		{Type: mbr.PART_EMPTY},
+		{Type: mbr.PART_EMPTY},
+	}
+
+	if len(partitions) != 4 {
+		return fmt.Errorf("4 partitions expected, got %d", len(partitions))
+	}
+
+	for i, spec := range partitionSpec {
+		if spec.Type != partitions[i].GetType() {
+			return fmt.Errorf("partition at index %d has type %v", i, partitions[i].GetType())
+		}
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/twitchyliquid64/raspberry-box
+
+require github.com/rekby/mbr v0.0.0-20190325193910-2b19b9cdeebc
